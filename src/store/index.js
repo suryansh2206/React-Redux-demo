e0aae4ba4e3ee723b958521d17574bdb2926b806
suryansh2206 <suryansh804.sg@gmail.com>
2023-05-18 13:33:24 +0530

    Auth reducer

--- src/store/index.js
+++ src/store/index.js
@@ -16,35 +16,26 @@ const counterSlice = createSlice({
   },
 });
 
-// const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
-//   if (action.type === "increase") {
-//     return {
-//       counter: state.counter + action.value,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "decrease") {
-//     return {
-//       counter: state.counter - action.value,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "toggle") {
-//     return {
-//       showCounter: !state.showCounter,
-//       counter: state.counter,
-//     };
-//   }
-
-//   return state;
-// };
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: {
+    isAuthenticated: false,
+  },
+  reducers: {
+    login: (state) => {
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+    },
+  },
+});
 
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;
